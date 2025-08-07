59c4b82021a3499bf157abcb1f5780f2ab34e9c2
nattyet-web <nattyskatea@gmail.com>
2025-08-07 10:20:32 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA5ZatulVEicBeWqNciKtjd4fcQeAvohmY",
   authDomain: "natty-caa61.firebaseapp.com",
   projectId: "natty-caa61",
-  storageBucket: "natty-caa61.appspot.com", // ✅ correct this!
+  storageBucket: "natty-caa61.appspot.com",
   messagingSenderId: "822585320498",
-  appId: "1:822585320498:web:01f76915ec4735bb6d082b",
-  measurementId: "G-3RVDQ36T50"
+  appId: "1:822585320498:web:01f76915ec4735bb6d082b"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-export const auth = getAuth(app);
