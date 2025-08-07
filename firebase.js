6276c5874f8f11a0751c99ca29741065531653af
nattyet-web <nattyskatea@gmail.com>
2025-08-07 09:11:16 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyA5ZatulVEicBeWqNciKtjd4fcQeAvohmY",
+  authDomain: "natty-caa61.firebaseapp.com",
+  projectId: "natty-caa61",
+  storageBucket: "natty-caa61.firebasestorage.app",
+  messagingSenderId: "822585320498",
+  appId: "1:822585320498:web:01f76915ec4735bb6d082b",
+  measurementId: "G-3RVDQ36T50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
